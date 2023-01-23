d863d5174e21207f9d7885ef40abde576f9b5869
Morgan Brown <mcbrown1890@gmail.com>
2023-01-23 18:18:40 -0500

    routing practice initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import {BrowserRouter, useParams, Routes, Route} from "react-router-dom";
+
+const ParamsComp = (props) => {
+  const {word, color, background} = useParams();
+  return(
+    <div>
+      {
+        isNaN(word) ? 
+        <p style={color?{color: color, backgroundColor: background}:null}>This is a word: {word}</p> : <p>This is a number: {word}</p>
+      }
+    </div>
+  )
+}
+
+const Home = (props) => {
+
+  return (
+      <div>
+          Welcome!
+      </div>
+  )
+}
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route element={<Home />} path="/home"/>
+        <Route element={<ParamsComp />} path="/:word"/>
+        <Route element={<ParamsComp />} path="/:word/:color/:background"/>
+      </Routes>            
     </div>
+    </BrowserRouter>
   );
 }
 
